439575a67086988e48543ad24e5820bb626cc6ef
Kidong Park <kdPark0723@gmail.com>
2018-09-08 19:55:38 +0900

    일렉트론 세팅

---
+++ main.js
@@ -0,0 +1,77 @@
+'use strict';
+
+// Electron 모듈 
+const path = require('path')
+const { app, BrowserWindow } = require('electron')
+
+if (process.mas) app.setName('ChatCode')
+
+// 메인 윈도우는 GC되지 않도록 글로벌 선언 
+let mainWindow = null
+
+const debug = /--debug/.test(process.argv[2])
+
+function initialize() {
+    const shouldQuit = makeSingleInstance()
+    if (shouldQuit) return app.quit()
+
+    app.on('ready', () => {
+        createMainWindow()
+    })
+
+    // 모든 창을 닫으면 종료 
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit()
+        }
+    })
+
+    app.on('activate', () => {
+        if (mainWindow === null) {
+            createMainWindow()
+        }
+    })
+}
+
+function createMainWindow() {
+    const windowOptions = {
+        width: 1080,
+        minWidth: 680,
+        height: 840,
+        title: app.getName(),
+        webPreferences: {
+            backgroundThorottling: false
+        }
+    }
+
+    mainWindow = new BrowserWindow(windowOptions)
+    mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
+
+    // Launch fullscreen with DevTools open, usage: npm run debug
+    if (debug) {
+        mainWindow.webContents.openDevTools()
+        mainWindow.maximize()
+        require('devtron').install()
+    } 
+    
+    mainWindow.once('ready-to-show', () => {
+        mainWindow.show()
+    })
+
+    mainWindow.on('closed', () => {
+        mainWindow = null
+    })
+}
+
+function makeSingleInstance() {
+    if (process.mas) return false
+
+    return app.makeSingleInstance(() => {
+        if (mainWindow) {
+            if (mainWindow.isMinimized()) mainWindow.restore()
+            mainWindow.focus()
+        }
+    })
+}
+
+initialize()
