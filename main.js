978d7baa9447291005be928d6bfe308655c88648
Kidong Park <kdPark0723@gmail.com>
2018-09-08 23:57:49 +0900

    스플레쉬 추가

--- main.js
+++ main.js
@@ -8,6 +8,7 @@ if (process.mas) app.setName('ChatCode')
 
 // 메인 윈도우는 GC되지 않도록 글로벌 선언 
 let mainWindow = null
+let splashWindow = null
 
 const debug = /--debug/.test(process.argv[2])
 
@@ -16,7 +17,10 @@ function initialize() {
     if (shouldQuit) return app.quit()
 
     app.on('ready', () => {
+        createSplashWindow()
         createMainWindow()
+        mainWindow.close()
+        splashWindow.close()
     })
 
     // 모든 창을 닫으면 종료 
@@ -55,14 +59,40 @@ function createMainWindow() {
         mainWindow.webContents.openDevTools()
         mainWindow.maximize()
         require('devtron').install()
-    } 
-    
-    mainWindow.once('ready-to-show', () => {
-        mainWindow.show()
-    })
+    }
 
     mainWindow.on('close', () => {
         mainWindow = null
+    })
+}
+
+function createSplashWindow() {
+    const windowOptions = {
+        width: 400,
+        minWidth: 400,
+        height: 400,
+        minheight: 400,
+        title: app.getName(),
+        transparent: true,
+        frame: false,
+
+        webPreferences: {
+            backgroundThorottling: false
+        }
+    }
+
+    splashWindow = new BrowserWindow(windowOptions)
+    splashWindow.loadURL(path.join('file://', __dirname, '/windows/splash.html'))
+
+    // Launch fullscreen with DevTools open, usage: npm run debug
+    if (debug) {
+        splashWindow.webContents.openDevTools()
+        splashWindow.maximize()
+        require('devtron').install()
+    } 
+
+    splashWindow.on('close', () => {
+        splashWindow = null
     })
 }
 
